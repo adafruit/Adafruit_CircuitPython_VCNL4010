2bf51887e2db38245572a6cbce8db502b92e7dd5
sommersoft <sommersoft@gmail.com>
2018-03-04 16:56:47 -0600

    sphinx fix; how did i miss that

--- adafruit_vcnl4010.py
+++ adafruit_vcnl4010.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 """
-`Adafruit_VCNL4010`
+`adafruit_vcnl4010`
 ====================================================
 
 CircuitPython module for the VCNL4010 proximity and light sensor.  See
